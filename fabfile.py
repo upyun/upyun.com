e8fa543ff75dd0ba1840f721d2c1df59151cc479
Nemo <zhhjchina@gmail.com>
2017-02-26 18:20:55 +0800

    Use rsync

--- fabfile.py
+++ fabfile.py
@@ -4,5 +4,4 @@ from fabric.api import *
 env.hosts = ['root@royldesign.com']
 def update():
   local('npm run build')
-  run('rm -rf /srv/upyun.com/dist')
-  local('scp -r dist root@royldesign.com:/srv/upyun.com')
+  local('rsync -r dist root@royldesign.com:/srv/upyun.com -avh --delete')
