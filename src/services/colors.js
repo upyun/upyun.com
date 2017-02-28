a7657784e6be961a0f4f0f579f5e3a77b1f72dd4
Nemo <zhhjchina@gmail.com>
2017-02-28 11:04:14 +0800

    Update color

--- src/services/colors.js
+++ src/services/colors.js
@@ -1,7 +1,7 @@
 var collection = {
   blue: { start: '#3AE7F3', end: '#1E96E5', mid: '#2EC0EC' },
   orange: { start: '#FFA254', end: '#FF768C', mid: '#ff8f6d' },
-  green: { start: '#10C7A0', end: '#01C8B1', mid: '#49d3d6' },
+  green: { start: '#10c7a0', end: '#01c8b1', mid: '#15a68d' },
   purple: { start: '#bc5ddd', end: '#807cea', mid: '#8461ca' },
   yellow: { start: '#ffc75a', end: '#ff701e', mid: '#ffa650' },
   darkBlue: { start: '#07D2F3', end: '#4945FF', mid: '#399ffb' }
