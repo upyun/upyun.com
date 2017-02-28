2c037ca3d6ff72efee93a5bc1007bb1818f2ab23
Nemo <zhhjchina@gmail.com>
2017-02-28 20:59:20 +0800

    Use amap instead of baidu map

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import './assets/global.sass'
 import up_gradient from './components/up_gradient'
 import up_button from './components/up_button'
 
+import AMap from 'vue-amap'
+Vue.use(AMap)
+AMap.initAMapApiLoader({
+  key: '7992a672d92cdf725ff50c351047a820',
+  plugin: ['AMap.OverView', 'AMap.ToolBar', 'AMap.MapType']
+})
+
 Vue.use(VueRouter)
 Vue.component('up_gradient', up_gradient)
 Vue.component('up_button', up_button)
--- src/pages/about/contact.vue
+++ src/pages/about/contact.vue
@@ -1,7 +1,25 @@
 <style lang="sass" src="./about.sass" scoped></style>
 <style lang="sass" scoped>
-  .cover
-    background-image: url(./assets/bg-3.jpg)
+@import "../../assets/susy"
+.cover
+  background-image: url(./assets/bg-3.jpg)
+.map-unit
+  +container
+  max-width: none
+  .map
+    +span(1 of 2 0)
+  .right
+    +span(1 of 2 0 last)
+    padding: 47px 54px
+    background: #fcfcfc
+    .title
+      margin-bottom: 20px
+      font-size: 23px
+      font-weight: 400
+    .caption
+      color: #97a1ab
+    p
+      margin: 8px 0 20px
 </style>
 
 <template lang="pug">
@@ -43,7 +61,7 @@ layout
               .content
                 a.icon.wechat
                 a.icon.weibo
-        .unit
+        .unit(style="padding-bottom: 0; border-bottom: 0")
           .title 公司地址
           .contact-address
             .bar
@@ -51,36 +69,42 @@ layout
               .item(:class="{active: active === 1}", @click="setMapPoint(1)") 北京分公司
               .item(:class="{active: active === 2}", @click="setMapPoint(2)") 上海分公司
               .item(:class="{active: active === 3}", @click="setMapPoint(3)") 广州分公司
-            #address_map(style="height: 464px")
-
+      .map-unit
+        el-amap.map(vid="amapDemo", :center="[addresses[active].x, addresses[active].y]", style="height: 464px", zoom="16", mapStyle="light", :scrollWheel="false", :plugin="['ToolBar']")
+          el-amap-marker(v-for='item in addresses', :position='[item.x, item.y]')
+        .right
+          .title 杭州总部
+          .content
+            .caption 公司地址：
+            p {{addresses[active].address}}
+            .caption 联系方式：
+            p {{addresses[active].phone}}
+            .caption 邮政编码：
+            p {{addresses[active].post}}
 </template>
 
 <script>
 import layout from './layout'
-import map from '../../services/map'
 
 export default {
   data () {
     return {
       map: null,
       addresses: [
-        { x: 120.095528, y: 30.295284, city: '杭州', name: '杭州总部', content: '地址： 杭州市西湖区文一西路 522号 西溪科创园 9号楼 5F<br/>电话： 0571-89966786<br/>邮编： 310000' },
-        { x: 116.422305, y: 39.944716, city: '北京', name: '北京分公司', content: '地址： 北京市东城区东四北大街 107 号科林大厦 B 座 511-512 室<br/>电话： 010-64016989<br/>邮编： 100080' },
-        { x: 121.443895, y: 31.243428, city: '上海', name: '上海分公司', content: '地址： 上海市普陀区安远路 518 号宝华城市晶典大厦 1308 室<br/>电话： 021-61231586<br/>邮编： 200060' },
-        { x: 113.37911, y: 23.132214, city: '广州', name: '广州分公司', content: '地址： 广州市天河区科韵路 12 号之一方圆E时光 1303 室<br/>电话： 020-29015900<br/>邮编： 510000' }
+        { x: 120.089138, y: 30.289631, city: '杭州', name: '杭州总部', address: '地址： 杭州市西湖区文一西路 522号 西溪科创园 9号楼 5F', phone: '0571-89966786', post: '310000' },
+        { x: 116.415577, y: 39.938624, city: '北京', name: '北京分公司', address: '地址： 北京市东城区东四北大街 107 号科林大厦 B 座 511-512 室', phone: '010-64016989', post: '100080' },
+        { x: 121.437556, y: 31.237, city: '上海', name: '上海分公司', address: '地址： 上海市普陀区安远路 518 号宝华城市晶典大厦 1308 室', phone: '021-61231586', post: '200060' },
+        { x: 113.372639, y: 23.126171, city: '广州', name: '广州分公司', address: '地址： 广州市天河区科韵路 12 号之一方圆E时光 1303 室', phone: '020-29015900', post: '510000' }
       ],
       active: 0
     }
   },
   mounted: function () {
-    map.init('address_map')
     this.setMapPoint(this.active)
   },
   methods: {
     setMapPoint: function (num) {
       this.active = num
-      var address = this.addresses[num]
-      map.setPoint(address)
     }
   },
   components: { layout }
