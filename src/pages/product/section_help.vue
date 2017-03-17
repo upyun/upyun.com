3bce021a24aa7b91e6f99244bb4f4f4afb39d2b3
Nicholas-Liu <liujun1861@126.com>
2017-03-17 14:24:24 +0800

    update documents of product_uss

--- src/pages/product/section_help.vue
+++ src/pages/product/section_help.vue
@@ -18,6 +18,10 @@
       +container()
       .menu
         +gallery(1 of 3 1)
+    &.uss
+      +container()
+      .menu
+        +gallery(1 of 4 1/2)
     width: 800px
     .menu
       font-size: 18px
@@ -81,6 +85,35 @@
         a(href="https://techs.b0.upaiyun.com/videos/cdnpage/zb-format.html" target="_blank") 转码与录制
         a(href="https://techs.b0.upaiyun.com/videos/cdnpage/andriod-player-sdk.html" target="_blank") 播放器相关
         a(href="https://techs.b0.upaiyun.com/videos/cdnpage/live_push_token.html" target="_blank") 防盗链相关
+    .menus.uss(v-if="selection === 'uss'")
+      .menu
+        .title 使用概述
+        a(href="http://docs.upyun.com/api/quick_start/" target="_blank") 快速入门
+        a(href="http://docs.upyun.com/api/developer_guide/#_4" target="_blank") 数据预处理
+        a(href="http://docs.upyun.com/api/developer_guide/#_5" target="_blank") 数据迁移
+        a(href="http://docs.upyun.com/api/rest_api/" target="_blank") REST API
+        a(href="http://docs.upyun.com/api/form_api/" target="_blank") FORM API
+        a(href="http://docs.upyun.com/api/fusion_storage/" target="_blank") 融合云存储
+      .menu
+        .title SDK
+        a(href="https://github.com/upyun/ios-sdk" target="_blank") iOS SDK
+        a(href="https://github.com/upyun/android-sdk" target="_blank") Android SDK
+        a(href="https://github.com/upyun/php-sdk" target="_blank") PHP SDK
+        a(href="https://github.com/upyun/java-sdk" target="_blank") Java SDK
+        a(href="https://github.com/upyun/python-sdk" target="_blank") Python SDK
+        a(href="https://github.com/upyun/go-sdk" target="_blank") Go SDK
+      .menu
+        .title 开发者工具
+        a(href="http://docs.upyun.com/api/developer_tools/#ftpftps" target="_blank") FTP/FTPS
+        a(href="http://docs.upyun.com/api/developer_tools/#upx" target="_blank") 命令行工具
+        a(href="http://docs.upyun.com/api/developer_tools/#curl" target="_blank") cURL
+      .menu
+        .title 常见问题
+        a(href="http://docs.upyun.com/faq/#ftp" target="_blank") FTP/FTPS
+        a(href="http://docs.upyun.com/cloud/spider/" target="_blank") 文件拉取
+        a(href="http://docs.upyun.com/cloud/unzip/" target="_blank") 压缩解压缩
+        a(href="https://techs.b0.upaiyun.com/videos/cdnpage/upyun_oss.html" target="_blank") 融合云存储配置
+        a(href="http://docs.upyun.com/api/errno/" target="_blank") API 错误码表
     .try
       up_button(:color="color", :url="$links.console", target="_blank") 免费试用
 </template>
