067aee22fce066758fbfa94174950a745100c281
Nicholas-Liu <liujun1861@126.com>
2017-04-17 16:19:52 +0800

    feat: add google_verify file and update customer's logo

--- build/build.js
+++ build/build.js
@@ -22,6 +22,7 @@ var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirect
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
 cp('-R', 'static/*', assetsPath)
+cp('googlec2479f8e37b3acfb.html', config.build.assetsRoot)
 
 webpack(webpackConfig, function (err, stats) {
   spinner.stop()
